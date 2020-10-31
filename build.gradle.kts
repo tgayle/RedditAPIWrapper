939fb722ae737eda2c7e45836b3852e04dbf5cc4
tgayle <travisgayledev@gmail.com>
2020-10-30 22:30:56 -0400

    Change Kotlin version to 1.4.10, add Retrofit/kotlinx.Serialization library.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.3.72"
+    kotlin("jvm") version "1.4.10"
+    kotlin("plugin.serialization") version "1.4.10"
 }
 
 group = "com.tgayle"
@@ -11,4 +12,14 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+
+    // Retrofit HTTP Client
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    // Retrofit Serializer for kotlin models to JSON
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
+
+    // Kotlin/JSON Serializer
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+
 }
