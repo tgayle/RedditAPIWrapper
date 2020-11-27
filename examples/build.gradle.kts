189141eeb7f9ba8da9051f4c30a49da3684f0a58
tgayle <travisgayledev@gmail.com>
2020-11-27 18:27:42 -0500

    feat(example): Update Compose version to 0.2.0-build132

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -2,8 +2,7 @@ import org.jetbrains.compose.compose
 
 plugins {
     kotlin("jvm")
-//    id("org.openjfx.javafxplugin") version "0.0.8"
-    id("org.jetbrains.compose") version "0.1.0-build63"
+    id("org.jetbrains.compose") version "0.2.0-build132"
     application
 }
 
@@ -16,22 +15,14 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
 
-//    implementation("no.tornado:tornadofx:1.7.20")
-//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-javafx:1.4.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.0")
 
     implementation(rootProject)
-    implementation(compose.desktop.all)
+    implementation(compose.desktop.currentOs)
 
 }
 
-//javafx {
-//    version = "11.0.2"
-//    modules = listOf("base", "controls", "graphics", "media", "web").map { "javafx.$it" }
-//}
-
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>() {
-//    kotlinOptions.jvmTarget = "1.8"
     kotlinOptions.jvmTarget = "1.8"
 }
 
